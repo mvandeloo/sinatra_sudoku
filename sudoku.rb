1593b6113182be13f167c040199088c635cfc5ae
mvandeloo <martin.vandeloo@googlemail.com>
2013-09-19 17:31:41 +0100

    commit 5 helpers included

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
+require_relative './helpers/application'
+
+enable :sessions
 
 def random_sudoku
     # we're using 9 numbers, 1 to 9, and 72 zeros as an input
@@ -13,12 +16,57 @@ def random_sudoku
     sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+	a = (0..80).to_a.sample(12)
+	a.each do |index|
+	sudoku[index] = ''
+	end
+	sudoku
+end
+
+
+# old:
+# get '/' do
+#   sudoku = random_sudoku
+#   session[:solution] = sudoku		
+#   @current_solution = puzzle(sudoku)
+#   erb :index
+# end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]    
+end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+end
+
 get '/' do
-  @current_solution = random_sudoku
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
   erb :index
 end
 
 
+get '/solution' do
+  @current_solution = session[:solution]
+  erb :index
+end
+
+post '/' do
+  cells = params["cell"]
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
+end
 
 
 
